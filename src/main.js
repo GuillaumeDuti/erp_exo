3fcc00806de1390f1aaf5b6ccd134c3252e16eba
GuillaumeDuti <dutilleuxguillaume@gmail.com>
2022-01-26 21:47:42 +0100

    instanciation Henry

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { Address } from "./models/address/Address.js";
+import { Client } from "./models/Client.js";
+
+let address = new Address("square Leopold", 15, 1040, "Brussels")
+let Henry = new Client("companyX", address, "0123", "Henryyy", "CEO", "0479020890" );
+
+window.Henry = Henry;
