efb1fcc37c9bac95bee1fa99ba76a673087bca19
GuillaumeDuti <dutilleuxguillaume@gmail.com>
2022-01-26 21:50:20 +0100

    newHenry

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import { Client } from "./models/Client.js";
 
 let address = new Address("square Leopold", 15, 1040, "Brussels")
 let Henry = new Client("companyX", address, "0123", "Henryyy", "CEO", "0479020890" );
-
+let newHenry = Henry;
 window.Henry = Henry;
+window.newHenry = newHenry;
