5c73e50927599ca57ffc27248226e57269648669
GuillaumeDuti <dutilleuxguillaume@gmail.com>
2022-01-27 14:27:57 +0100

    mailBox

--- src/models/address/Address.js
+++ src/models/address/Address.js
@@ -3,9 +3,12 @@ export class Address {
     number;
     postalCode;
     city;
-    constructor(street, number, postalCode, city ) {
+    mailBox;
+
+    constructor(street, number, mailBox = undefined , postalCode, city ) {
         this.street     = street;
         this.number     = number;
+        this.mailBox    = mailBox;
         this.postalCode = postalCode;
         this.city       = city;
     }
